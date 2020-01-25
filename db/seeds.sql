41b3ef50018995971932df88a54a4dfcacaa4e6a
Tcauley91 <caleytyler@gmail.com>
2020-01-24 21:53:35 -0500

    files created and cats references added

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Beef Burger');
